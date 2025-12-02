1ec305174cf0bb5b71449e25807adc097e8b505f
Saumya Suripeddi <saumyasuripeddi@gmail.com>
2025-12-01 20:40:42 -0500

    A6 mongoose fixed

--- Kambaz/Assignments/dao.js
+++ Kambaz/Assignments/dao.js
@@ -1,15 +1,42 @@
-import moduleSchema from "../Modules/schema.js";
-import assignmentSchema from "../Assignments/schema.js";
+import { v4 as uuidv4 } from "uuid";
+import CourseModel from "../Courses/model.js";
 
-const courseSchema = new mongoose.Schema({
-  _id: String,
-  name: String,
-  number: String,
-  startDate: String,
-  endDate: String,
-  department: String,
-  credits: Number,
-  description: String,
-  modules: [moduleSchema],
-  assignments: [assignmentSchema],
-});
+export default function AssignmentsDao() {
+  async function findAssignmentsForCourse(courseId) {
+    const course = await CourseModel.findById(courseId);
+    return course.assignments || [];
+  }
+
+  async function createAssignment(courseId, assignment) {
+    const newAssignment = { ...assignment, _id: uuidv4() };
+
+    await CourseModel.updateOne(
+      { _id: courseId },
+      { $push: { assignments: newAssignment } }
+    );
+
+    return newAssignment;
+  }
+
+  async function deleteAssignment(courseId, assignmentId) {
+    return CourseModel.updateOne(
+      { _id: courseId },
+      { $pull: { assignments: { _id: assignmentId } } }
+    );
+  }
+
+  async function updateAssignment(courseId, assignmentId, assignmentUpdates) {
+    const course = await CourseModel.findById(courseId);
+    const assignment = course.assignments.id(assignmentId);
+    Object.assign(assignment, assignmentUpdates);
+    await course.save();
+    return assignment;
+  }
+
+  return {
+    findAssignmentsForCourse,
+    createAssignment,
+    deleteAssignment,
+    updateAssignment,
+  };
+}
