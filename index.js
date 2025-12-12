81217e96fbaad7681b055e10eccfe407b96c2466
Saumya Suripeddi <saumyasuripeddi@gmail.com>
2025-12-12 12:31:44 -0500

    fix CORS with regex for all Vercel domains

--- index.js
+++ index.js
@@ -33,11 +33,7 @@ app.use(
     credentials: true,
     origin: [
       "http://localhost:3000",
-      "https://kambaz-next-6567c8jp5-saumya-suripeddis-projects.vercel.app",
-      "https://kambaz-next-js-saumyasuripeddi-5320-saumya-suripeddis-projects.vercel.app",
-      "https://kambaz-next-609pu5j25-saumya-suripeddis-projects.vercel.app",
-      "https://kambaz-next-3jkvlhb04-saumya-suripeddis-projects.vercel.app",
-      "https://kambaz-next-gpvrj6wlg-saumya-suripeddis-projects.vercel.app"
+      /\.vercel\.app$/  
     ],
   })
 );
